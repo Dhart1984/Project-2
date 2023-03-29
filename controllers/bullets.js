0ce8ad168305302fc5ee0cb964da31fea7df45aa
Jose Aguirre <aguirrejose615@gmail.com>
2023-03-29 10:18:41 -0400

    fixed bugs and add journal page

--- controllers/bullets.js
+++ controllers/bullets.js
@@ -2,8 +2,8 @@ const Bullet = require('../models/Bullet');
 
 
 function newBullet(req, res){
-  Task.find({}).then(function(bullets){
-    res.render('bullets/new', {title: 'Enter a new Bullet', bullets})
+  Bullet.find({}).then(function(bullets){
+    res.render('tasks/bullets', {title: 'Enter a new Bullet', bullets})
 
 
   }) .catch(function (err) {
@@ -15,7 +15,6 @@ module.exports = {
   new: newBullet,
   create,
   index,
-  show,
   delete: deleteBullet
 }
 
@@ -23,7 +22,7 @@ module.exports = {
 function create(req, res) {
   Bullet.create(req.body).then(function (newBullet) {
       console.log(newBullet)
-      res.redirect('/bullets/new')
+      res.redirect('/bullets')
   })
   console.log(req.body)
 }
@@ -37,21 +36,6 @@ function index(req, res) {
           console.log(err) // log the error for debugging or redirect to error page 
           res.redirect('/')
       })
-}
-
-
-
-
-function show(req, res) {
-  Bullet.findById(req.params.id).then(
-      function (bullet) {
-          res.render('bullets/show', { state, title: 'Bullet Details' })
-      })
-      .catch(function (err) {
-          console.log(err)
-          res.redirect('/')
-      })
-  console.log("this function is doing something ")
 }
 
 function deleteBullet(req, res){
